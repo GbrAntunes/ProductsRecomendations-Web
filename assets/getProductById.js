9ab9489c0aef34c44f5e2c48b13a152d50879aca
Gabriel Antunes <gbr.antunes@gmail.com>
2020-11-09 22:35:43 -0400

    API's IP changed to localhost

--- assets/getProductById.js
+++ assets/getProductById.js
@@ -1,5 +1,5 @@
 async function getProductById(id) {
-  const apiResponse = await fetch(`http://192.168.15.28:3333/products/${id}`)
+  const apiResponse = await fetch(`http://localhost:3333/products/${id}`)
 
   return apiResponse.status === 404 ? false : apiResponse.json()
 }
--- assets/getProductIdList.js
+++ assets/getProductIdList.js
@@ -1,5 +1,5 @@
 async function getProductIdList() {
-  const apiResponse = await fetch('http://192.168.15.28:3334?maxProducts=16')
+  const apiResponse = await fetch('http://localhost:3334?maxProducts=16')
 
   return apiResponse.json()
 }
